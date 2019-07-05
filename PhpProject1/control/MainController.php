91e06ab8788489e177f3012d763eb84ee6923abe
Airton <36297249+airtoncuri@users.noreply.github.com>
2019-07-05 01:07:59 -0300

    Add files via upload

--- PhpProject1/control/MainController.php
+++ PhpProject1/control/MainController.php
@@ -1,33 +1,57 @@
 <?php
 
+include_once "FilmeController.php";
+
+include_once "LocacaoController.php";
+
 include_once "SocioController.php";
+
+include_once "Item_LocacaoController.php";
 
 /**
  * Descreva aqui a classe MainController
  *
  * @author airton
  */
-class MainController {
+class mainController {
     
+    private $filmeController;
+    private $locacaoController;
     private $socioController;
+    private $item_LocacaoController;
     
     public function __construct() {
+    $this->filmeController = new FilmeController();
+    $this->locacaoController = new LocacaoController();
     $this->socioController = new SocioController();
-   
+    $this->item_LocacaoController = new Item_LocacaoController();
         }
     
     public function exibirMenu(){
         $exit = 1;
         while ($exit != 0){
             echo "\n\n--------- Menu ---------";
-            echo "\n1. Manter Socio";
+            echo "\n1. Manter Filme";
+            echo "\n2. Manter Locacao";
+            echo "\n3. Manter Socio";
+            echo "\n4. Manter Item de Locacao";
             echo "\nOpção (ZERO para sair): ";
             $exit = fgets(STDIN);
             switch ($exit){
                 case 0:
                     break;
                 case 1:
+                    $this->filmeController->exibeMenu();
+                    break;
+                case 2:
+                    $this->locacaoController->exibeMenu();
+                    break;
+                case 3:
                     $this->socioController->exibeMenu();
+                    break;
+                case 4:
+                    $this->locacaoController->exibeMenu();
+                    break;
                 default:
                     echo "\nOpção inexistente.";
             }
@@ -35,7 +59,6 @@ class MainController {
     }
         
 }
-
 
 
 $mainController = new MainController();
